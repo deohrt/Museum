73dd8d9097eec07a4108a7a0e8933d9e2aa25111
deohrt <deohartanto@mhs.mdp.ac.id>
2023-01-03 16:27:05 +0700

    Initial commit

---
+++ app/src/main/java/com/if3b/museum/DataMuseum.java
@@ -0,0 +1,37 @@
+package com.if3b.museum;
+
+import java.util.ArrayList;
+
+public class DataMuseum {
+
+    public static String[] [] data = new String[][] {
+            {"Museum Negeri Gayo","Masyarakat Gayo memiliki sejarah dan budaya yang lebih spesifik, sehingga diperlukan pembangunan Museum Negeri Gayo sebagi tempat menyimpan koleksi benda-benda sejarah Gayo itu. Dengan adanya museum ini, para wisatawan, ilmuwan dan pemerhati budaya dapat melihat benda bersejarah untuk meneliti sejarah masyarakat Gayo. Koleksi mahakarya di Museum Negeri Gayo berupa benda-benda peninggalan sejarah dan pra-sejarah Gayo yang ditemukan di Loyang Mendale dan Ujung Karang oleh peneliti Balai Arkeologi Medan. Benda-benda tersebut antara lain berupa peninggalan artefak dan ekofak dari kehidupan pra-sejarah pada masyarakat Gayo .","https://www.andalastourism.com/history/wp-content/uploads/2021/10/Alamat-Museum-Negeri-Gayo.jpg"},
+            {"Museum Islam Samudra Pasai","Kerajaan Samudera Pasai adalah kerajaan islam yang di nusantara yang memiliki banyak peninggalan sejarah- sejarah beradaban lama yang berupa benda benda bersejarah yang akan terus dijaga dan dipelihara guna bermanfaat kepada anak cucuk kela,benda- benda bersejarah ini dijaga oleh pemerintah aceh khususnya kota banda aceh.","https://www.simbun.com/wp-content/uploads/2018/02/Museum-Islam-Samudra-Pasai.-Foto-Irman-1024x682.jpg"},
+            {"Museum Ali Hasjmy","Khutubkhanah Tgk Chik Kutakarang yang berisi kitab-kitab dan buku dari berbagai disiplin ilmu baik agama, sastra dan sejarah, termasuk buku-buku dari awal abad ke 20.\n" +
+                    "Selanjutnya adalah ruangan Warisan Budaya Nenek Puteh yang berisikan benda budaya dari Aceh seperti pakaian adat Aceh, benda-benda keramik masa silam dan juga senjata-senjata khas Aceh. Di ruangan tersebut juga ada pedang milik Habib Mustafa, pahlawan Aceh yang meninggal saat melawan Belanda di Bakongan, Aceh Selatan, tahun 1926.\n" +
+                    "Khasanah Ali Hasyimy yang memperkenalkan sosok beliau sepanjang hidupnya. Di sana tersimpan rapi dokumen-dokumen pribadi semasa sekolah maupun saat menjadi gubernur dan Menteri Dalam Negeri Indonesia (1964 – 1966).\n" +
+                    "Ruangan yang terakhir adalah Tekonologi Tradisonal Aceh yang menyimpan hasil produksi kerajinan rakyat Aceh.","https://www.andalastourism.com/history/wp-content/uploads/2022/07/Museum-Raja-Ali-Haji.jpg"},
+            {"Museum Pulau Onrust","adalah lokasi wisata sejarah di Pulau Onrust yang didalamnya meliputi beberapa tempat bersejarah seperti museum Onrust yang merupakan bekas rumah sakit, bekas bangunan barak karantina haji, sumur tempat penampungan air bersih, bekas penjara, makam belanda, makam pribumi dan makam keramat.","https://1.bp.blogspot.com/-vYpvyQCNa8s/YH-tpNQ80zI/AAAAAAAAGGs/1Y7EfHtkDf4M6If_JzYQbHFaWRl5bnywACLcBGAsYHQ/w640-h360/1.jpg"},
+            {"Graha Bhakti Antara","Gedung ini dibangun pada awal abad ke-20 Pada tanggal 17 Agustus 1945, Adam Malik datang ke kantor Domei mendiktekan naskah proklamasi yang baru saja dibacakan Bung Karno, tanpa menghiraukan izin dari sensor Jepang (Hondokan) seperti lazimnya. Maka berita tadi oleh Pangulu Lubis dikirim ke bagian radio dan diselipkan di antara berita-berita lain. Markonis Soegiri bertugas mengawasi pelaksanaan tersiarnya teks Proklamasi tersebut dan pengirimannya dilaksanakan oleh Markonis Wua.","https://0d53a35ccf6c1b73db42-2ab649be0a06e8d138dbd96e3457e3f2.ssl.cf2.rackcdn.com/289556.jpg"},
+            {"Museum Tamansiswa Dewantara Kirti Griya","Bangunan rumah yang berdiri di atas tanah seluas 5.594 m2 tersebut dibeli atas nama Ki Hadjar Dewantara, Ki Sudaminto, Ki Supratolo dari Mas Adjeng Ramsinah pada tanggal 14 Agustus 1935. Konon bangunan rumah tersebut didirikan pada tahun 1925 dengan gaya Jawa. Bangunan tercatat dalam buku register Kraton Ngayogyakarta tertanggal 26 Mei 1926, dengan nomor Angka 1383 / l.H (2). Pada tanggal 18 Desember 1951, pembelian tersebut dihibahkan kepada Yayasan Persatuan Perguruan Tamansiswa.","https://id.wikipedia.org/wiki/Museum_Dewantara_Kirti_Griya#/media/Berkas:Pendapa_tamansiswa.jpg"},
+            {"Museum Sonobudoyo","ejarah dan kebudayaan Jawa, termasuk bangunan arsitektur klasik Jawa. Museum ini menyimpan koleksi mengenai budaya dan sejarah Jawa yang dianggap paling lengkap setelah Museum Nasional Republik Indonesia di Jakarta. Selain keramik pada zaman Neolitik dan patung perunggu dari abad ke-8, museum ini juga menyimpan beberapa macam bentuk wayang kulit, berbagai senjata kuno (termasuk keris dan topeng Jawa).","https://id.wikipedia.org/wiki/Museum_Sonobudoyo#/media/Berkas:Museum_Sonobudoyo.JPG"},
+            {"Museum Monumen Pangeran Diponegoro","Museum ini menempati bekas kediaman Pangeran Diponegoro dan keluarganya. Pembangunan Museum (monumen) ini diprakarsai oleh Mayjen TNI Surono, yang kemudian dilanjutkan oleh Mayjen TNI Widodo. Berdasarkan Surat Keputusan Pangdam VII Diponegoro No. 99/7/1968, tanggal 2 Juli 1968 dibentuklah panitia persiapan perencanaan pelaksanaan pembangunan Monumen Pahlawan Pangeran Diponegoro di bekas rumah kediaman Pangeran Diponegoro di Tegalrejo, Yogyakarta. Ahli waris Pangeran Diponegoro menyetujui jika tanah peninggalan dia didirikan monumen. Surat psrnyataan tersebut ditandatangani oleh KRT. Prodjodiningrat, Nyi Hajar Dewanfara, dan dr. Sahir Nitihardjo (RA. Kajafin Diponegoro).","https://id.wikipedia.org/wiki/Museum_Monumen_Pangeran_Diponegoro#/media/Berkas:Museum_Monumen_Pangeran_Diponegoro.jpg"},
+            {"Museum Kailasa","merupakan sebuah museum di Dataran Tinggi Dieng, Jawa Tengah. Museum yang memiliki 4 bangunan utama ini memiliki koleksi benda-benda cagar budaya, artefak, dan film mengenai Dieng. Koleksi yang dapat dilihat pada museum ini di antaranya memuat sejarah letusan Gunung Prahu Tua, sumber batu andesit yang digunakan untuk pembangunan candi, dan sistem kepercayaan masyarakat Dieng.","https://id.wikipedia.org/wiki/Museum_Kailasa#/media/Berkas:Museum_Dieng_Kailasa.jpg"},
+            {"Museum Bank Rakyat Indonesia","adalah museum yang terletak di kota Purwokerto dan berisi tentang sejarah berdirinya Bank Rakyat Indonesia.","https://id.wikipedia.org/wiki/Museum_Bank_Rakyat_Indonesia#/media/Berkas:Bank_BRI_Museum_Purwokerto.jpg"},
+
+    };
+
+    public static ArrayList<ModelMuseum> ambilDataPahlawan() {
+        ArrayList<ModelMuseum> dataMuseum = new ArrayList<>();
+        for (String[] varData : data) {
+            ModelMuseum model = new ModelMuseum();
+            model.setNama(varData[0]);
+            model.setTentang(varData[1]);
+            model.setFoto(varData[2]);
+
+            dataMuseum.add(model);
+        }
+
+        return dataMuseum;
+    }
+}
